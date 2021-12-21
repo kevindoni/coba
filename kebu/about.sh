708a931ccf7c679efea3f26bea912d1d694fcb94
kevindoni <kevindoni17@gmail.com>
2021-12-21 20:31:55 +0700

    Add files via upload

---
+++ kebu/about.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#     Premium Auto Script By KEVIN DONI         #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" 
+echo -e "# For VPS with KVM and VMWare virtualization    #" 
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Thanks To  :                                  #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# ALLAH S.W.T                                   #" 
+echo -e "# My Family                                     #" 
+echo -e "# Google                                        #" 
+echo -e "# Admin Portal & White Vps                      #" 
+echo -e "# Tuan YZ                                       #" 
+echo -e "# Adisubagja                                    #"
+echo -e "# Demonk                                        #" 
+echo -e "#                                               #" 
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Rebuild By : KEVIN DONI               #" 
+echo -e "# Contact    : t.me/kevindoni                  #" 
+echo -e "# Whatsapp   : 082243698398                     #" 
+echo -e "=================================================" | lolcat
